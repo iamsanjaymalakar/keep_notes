91c46244e14486788956f7ad7fa164a1f4d2d497
Sanjay Malakar <iamsanjaymalakar@gmail.com>
2020-05-28 17:30:30 +0600

    footer

---
+++ keep_react/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            ⓒ Sanjay Malakar {new Date().getFullYear()}
+        </footer>
+    );
+}
+
+export default Footer;
